70c5ae93b2720638763358920990183f9e06a2d6
Lyndon Sanche <lsanche@lyndeno.ca>
2022-06-13 18:37:31 -0600

    Turn into flake

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "Lyndon Sanche's resume";
+
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+    flake-utils.url = github:numtide/flake-utils;
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+  with flake-utils.lib; eachSystem allSystems (system:
+  let
+    pkgs = nixpkgs.legacyPackages.${system};
+    tex = pkgs.texlive.combine {
+      inherit (pkgs.texlive)
+        scheme-basic latex-bin latexmk titlesec fontawesome
+        fontspec xcolor dashrule ifmtarg enumitem hyphenat lato fontaxes
+        xkeyval;
+    };
+  in rec {
+    packages = {
+      resume = pkgs.stdenvNoCC.mkDerivation rec {
+        name = "lsanche-resume";
+        src = self;
+        buildInputs = [ pkgs.coreutils tex ];
+        phases = [ "unpackPhase" "buildPhase" "installPhase" ];
+        buildPhase = ''
+          export PATH="${pkgs.lib.makeBinPath buildInputs}";
+          mkdir -p .cache/texmf-var
+          env TEXMFHOME=.cache TEXMFVAR=.cache/texmf-var \
+            latexmk -interaction=nonstopmode -pdf -lualatex \
+            lsanche.tex
+        '';
+        installPhase = ''
+          mkdir -p $out
+          cp lsanche.pdf $out/
+        '';
+      };
+    };
+    defaultPackage = packages.resume;
+  });
+}
