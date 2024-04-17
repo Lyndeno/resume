d23f577faf91459fa11a915d666880ad593c31fa
Lyndon Sanche <lsanche@lyndeno.ca>
2024-04-16 19:15:11 -0600

    Remove hunspell hook as it does not work

--- flake.nix
+++ flake.nix
@@ -13,52 +13,52 @@
     flake-utils,
     pre-commit-hooks,
   }:
-    with flake-utils.lib; eachSystem defaultSystems (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-      tex = pkgs.texlive.withPackages (ps:
-        with ps; [
-          scheme-basic
-          latex-bin
-          latexmk
-          titlesec
-          fontawesome
-          fontspec
-          xcolor
-          dashrule
-          ifmtarg
-          enumitem
-          hyphenat
-          lato
-          fontaxes
-          xkeyval
-        ]);
-    in rec {
-      checks = {
-        pre-commit-check = pre-commit-hooks.lib.${system}.run {
-          src = ./.;
-          hooks = {
-            alejandra.enable = true;
-            hunspell.enable = true;
+    with flake-utils.lib;
+      eachSystem defaultSystems (system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+        tex = pkgs.texlive.withPackages (ps:
+          with ps; [
+            scheme-basic
+            latex-bin
+            latexmk
+            titlesec
+            fontawesome
+            fontspec
+            xcolor
+            dashrule
+            ifmtarg
+            enumitem
+            hyphenat
+            lato
+            fontaxes
+            xkeyval
+          ]);
+      in rec {
+        checks = {
+          pre-commit-check = pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              alejandra.enable = true;
+            };
           };
         };
-      };
-      packages = {
-        resume = pkgs.stdenvNoCC.mkDerivation rec {
-          name = "lsanche-resume";
-          src = self;
-          buildInputs = [pkgs.gnumake pkgs.coreutils tex];
-          phases = ["unpackPhase" "buildPhase" "installPhase"];
-          installPhase = ''
-            mkdir -p $out
-            cp lsanche.pdf $out/
-          '';
+        packages = {
+          resume = pkgs.stdenvNoCC.mkDerivation rec {
+            name = "lsanche-resume";
+            src = self;
+            buildInputs = [pkgs.gnumake pkgs.coreutils tex];
+            phases = ["unpackPhase" "buildPhase" "installPhase"];
+            installPhase = ''
+              mkdir -p $out
+              cp lsanche.pdf $out/
+            '';
+          };
+          default = packages.resume;
         };
-        default = packages.resume;
-      };
-      devShells = {
-        default = pkgs.mkShell {
-          buildInputs = [packages.default.buildInputs];
+        devShells = {
+          default = pkgs.mkShell {
+            buildInputs = [packages.default.buildInputs];
+          };
         };
-      };
-    });
+      });
 }
